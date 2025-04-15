8e9e0e465b747117455d17dad33a352ab41c6a83
ArtthurFernandes <arthurbr1412@gmail.com>
2025-04-15 11:56:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+  <body>
+    
+    <header>
+  <nav>
+     <ul class="navegacao">
+        <li>
+           <p>
+             <a href="#" class="ifbooks">IFbooks</a>
+           </p>
+        </li>
+        <li>
+          <span>Apreço a leitura</span>
+        </li>
+        <li>
+            <a href="#">Termos</a>
+        </li>
+        <li>
+          <a href="#">Equipe</a>
+        </li>
+        <li>
+          <a href="#">Envio</a>
+        </li>
+        <li>
+          Devoluções
+        </li>
+        <li>
+        </li>
+     </ul>
+     <div>
+
+     </div>
+  </nav>
+</header>
+  </body>
+
+</template>
+
+<style scoped>
+
+</style>
