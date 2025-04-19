2ab19f6e8ddc82835388c91716a0c2512211b58a
adrio.oliveira <adriojoliveira@gmail.com>
2025-04-18 21:55:00 -0300

    atv figma

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
 <template>
   <body>
     
-    <header>
-  <nav>
-     <ul class="navegacao">
+    <main>
+      <header>
+  <nav class="navegacao">
+   
+     <ul>
         <li>
            <p>
              <a href="#" class="ifbooks">IFbooks</a>
            </p>
         </li>
+        <div class="pauzinho1"></div>
         <li>
-          <span>Apreço a leitura</span>
+          <span>Apreço a <br> leitura</span>
         </li>
         <li>
             <a href="#">Termos</a>
@@ -26,18 +29,43 @@
           <a href="#">Envio</a>
         </li>
         <li>
-          Devoluções
+          <a href="#">Devoluções</a>
+        
         </li>
-        <li>
-        </li>
-     </ul>
-     <div>
-
-     </div>
+     
+        <div class="menu-icones">
+      <img src="@/assets/carrinho.png" alt="carrinho" class="icon" />
+      <div class="pauzinho"></div>
+      <img src="@/assets/Heart.png" alt="coração" class="icon" />
+      <div class="pauzinho"></div>
+      <img src="@/assets/User.png" alt="usuário" class="icon" />
+    </div>
+     </ul> 
   </nav>
 </header>
+          <section>
+            <div>
+              <h4>
+                Autor de abril
+              </h4>    
+              <h1>
+                  Eric-Emanuel Schmitt
+              </h1>
+                <p>
+                  Eric-Emmanuel Schmitt has been awarded more than 20 <br>  literary prizes and distinctions, and in 2001 he received the <br>  title of Chevalier des Arts et des Lettres. His books have been <br>  translated into over 40 languages.
+                </p>
+                <button>Acessar página do livro</button>
+            </div>
+            <div class="livro">
+              <img src="/src/assets/book.png" alt="book.png">
+            </div>
+          </section>
+          <section>
+                
+          </section>
+         
+    </main>     
   </body>
-
 </template>
 
 <style scoped>
