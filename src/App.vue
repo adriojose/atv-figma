3fbd50116fc752f9635f82804d70e3bf5c53a86e
adrio.oliveira <adriojoliveira@gmail.com>
2025-04-19 22:58:24 -0300

    atv figma

--- src/App.vue
+++ src/App.vue
@@ -60,8 +60,39 @@
               <img src="/src/assets/book.png" alt="book.png">
             </div>
           </section>
+          <section class="borda">
+                <div class="selos">
+                  <img src="/src/assets/Truck.png" alt="truck.png" class="selo">
+                  <div class="texto">Frete grátis para SC</div>
+                  <div class="traco"></div>
+                  <img src="/src/assets/Star.png" alt="star.png" class="selo">
+                  <div class="texto">Livros recomendados</div>
+                  <div class="traco"></div>
+                  <img src="/src/assets/Book open.png" alt="book.png" class="selo">
+                  <div class="texto2">Mais vendidos</div>   
+                </div>
+          </section>
+          <div class="borda2"></div>
           <section>
-                
+                <h2>
+                    Lançamentos
+                </h2>
+                <div class="livros">
+                   <div class="livro1">
+                    <img src="/src/assets/livro.png" alt="livro.png">
+                    <h3>Chain of Iron: Volume 2</h3>
+                    <p>Cassandra Clare</p>
+                    <p>R$23.24</p>
+                  <button class="button2">comprar</button>
+                   </div>   
+                   <div class="livro1">
+                    <img src="/src/assets/livro2.png" alt="livro2.png">
+                    <h3>Chain of Thorns</h3>
+                    <p>Cassandra Clare</p>
+                    <p>R$23.24</p>
+                   <button class="button2">comprar</button>
+                   </div>
+                </div>                
           </section>
          
     </main>     
